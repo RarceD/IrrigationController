ff12f3cf87a3caadb7e3114c9354015231f73434
RarceD <akimbo170@gmail.com>
2020-03-19 17:10:28 +0100

    interacting with the PG

--- Testing_Tecnology/serial_open_close_valves_port/serial_open_close_valves_port.ino
+++ Testing_Tecnology/serial_open_close_valves_port/serial_open_close_valves_port.ino
@@ -10,6 +10,10 @@ static uint8_t cmd_start_manprg[] = {0x02, 0xfe, 'S', 'T', 'A', 'R', 'T', ' ', '
 static uint8_t cmd_start_manvalv[] = {0x02, 0xfe, 'S', 'T', 'A', 'R', 'T', ' ', 'M', 'A', 'N', 'V', 'A', 'L', 'V', 0x23, '0', '1', 0x23, '0', '0', '0', '1', 0x23, 0x03, 0, 0};
 static uint8_t cmd_stop_manvalv[] = {0x02, 0xfe, 'S', 'T', 'O', 'P', ' ', 'M', 'A', 'N', 'V', 'A', 'L', 'V', 0x23, ' ', ' ', 0x23, 0x03, 0, 0};
 
+static uint8_t cmd_read_line[] = {0x02, 0xfe, 'R', 'E', 'A', 'D', ' ', 'L', 'I', 'N', 'E', 0x23, '4', '0', '0', 0x23, 0x03, 0, 0};
+
+static uint8_t cmd_write_data[] = {0x02, 0xfe, 'W', 'R', 'I', 'T', 'E', ' ', 'D', 'A', 'T', 'A', 0x23, ' ', ' ', ' ', 0x23, ' ', ' ', 0x23, 0x03, 0, 0};
+
 uint16_t i, j;
 int calcrc(char ptr[], int length);
 void open_valve_pg(bool state, uint8_t valve, uint8_t time_hours, uint8_t time_minutes);
@@ -21,22 +25,24 @@ void setup()
 
   Serial.begin(115200);
   softSerial.begin(9600);
-  calcrc((char *)buf, sizeof(buf) - 2);
-  // softSerial.write(buf, sizeof(buf));
+  Serial.print(time_to_pg_format(2, 5), HEX);
+  calcrc((char *)cmd_read_line, sizeof(cmd_read_line) - 2);
+  softSerial.write(cmd_read_line, sizeof(cmd_read_line));
   Serial.print("Command send: ");
-  for (i = 0; i < sizeof(buf); i++)
+  for (i = 0; i < sizeof(cmd_read_line); i++)
   {
     Serial.print(buf[i], HEX);
     Serial.print(" ");
   }
-  for (i = 0; i < sizeof(buf); i++)
+  Serial.println(" ");
+
+  for (i = 0; i < sizeof(cmd_read_line); i++)
   {
-    Serial.write(buf[i]);
+    Serial.write(cmd_read_line[i]);
     Serial.print(" ");
   }
   Serial.println();
   Serial.print("Read response: ");
-  delay(2000);
 }
 
 /******************************************************************* main program  ************************************************************************************/
@@ -57,6 +63,57 @@ void loop()
       delay(10000);
       Serial.println("YA");
       action_prog_pg(false, 'C');
+    }
+    if (a == 100) //Press the D for change PG memmory starts
+    {
+      // 13   14   15  the position in memmory
+      // 17   18       what I write
+      cmd_write_data[13] = '4';
+      cmd_write_data[14] = '0';
+      cmd_write_data[15] = '0';
+
+      cmd_write_data[17] = '4';
+      cmd_write_data[18] = '9';
+      calcrc((char *)cmd_write_data, sizeof(cmd_write_data) - 2);
+      softSerial.write(cmd_write_data, sizeof(cmd_write_data));
+      for (i = 0; i < sizeof(cmd_write_data); i++)
+      {
+        Serial.print(cmd_write_data[i], HEX);
+        Serial.print(" ");
+      }
+    }
+    if (a == 101) //Press the E for change PG memmory starts
+    {
+      // 13   14   15  the position in memmory
+      // 17   18       what I write
+      cmd_write_data[13] = '4';
+      cmd_write_data[14] = '0';
+      cmd_write_data[15] = '0';
+      cmd_write_data[17] = '4';
+      cmd_write_data[18] = '0';
+      calcrc((char *)cmd_write_data, sizeof(cmd_write_data) - 2);
+      softSerial.write(cmd_write_data, sizeof(cmd_write_data));
+      for (i = 0; i < sizeof(cmd_write_data); i++)
+      {
+        Serial.print(cmd_write_data[i], HEX);
+        Serial.print(" ");
+      }
+    }
+       if (a == 102) { // press F change starts
+      // 13   14   15  the position in memmory
+      // 17   18       what I write
+      cmd_write_data[13] = '1';
+      cmd_write_data[14] = 'A';
+      cmd_write_data[15] = '1';
+      cmd_write_data[17] = '3';
+      cmd_write_data[18] = 'B';
+      calcrc((char *)cmd_write_data, sizeof(cmd_write_data) - 2);
+      softSerial.write(cmd_write_data, sizeof(cmd_write_data));
+      for (i = 0; i < sizeof(cmd_write_data); i++)
+      {
+        Serial.print(cmd_write_data[i], HEX);
+        Serial.print(" ");
+      }
     }
   }
 
@@ -159,3 +216,30 @@ void action_prog_pg(bool state, char program)
     softSerial.write(cmd_stop_manprg, sizeof(cmd_stop_manprg));
   }
 }
+uint8_t time_to_pg_format(uint8_t hours, uint8_t minutes)
+{
+  if (hours == 0) //Less than an hour I just have to convert to hex:
+    return minutes;
+  else
+  {
+    //I obtein the units of the number and then decimal
+    uint8_t min_units = minutes - (minutes / 10) * 10;
+    if (min_units >= 5)
+      min_units = 5;
+    else
+      min_units = 0;
+    minutes = (minutes / 10) * 10 + min_units;
+    hours--;
+    //Now I have the time in a correct format and I can convert it
+    uint8_t times_minutes = 0;
+    while (minutes > 0)
+    {
+      minutes -= 5;
+      times_minutes++;
+    }
+    return (60 + 12 * hours + times_minutes);
+  }
+}
+// void write_PG_starts(char program, uint8_t start_hours, uint8_t start_min)
+// {
+// }
